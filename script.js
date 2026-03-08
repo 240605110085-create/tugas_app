9efdf29613a27dd581c20fcc6e19720a4e7b3c86
Rayya Naura Zahira <240605110085@student.uin-malang.ac.id>
2026-03-08 22:20:00 +0700

    update kode

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
-function addTask(){
-let input = document.getElementById("taskInput");
-let taskText = input.value;
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let deadline = document.getElementById("deadlineInput");
 
-if(taskText === "") return;
+    let taskText = input.value;
+    let deadlineDate = deadline.value;
 
-let li = document.createElement("li");
+    if (taskText === "") return;
 
-li.innerHTML = `
+    let li = document.createElement("li");
+
+    li.innerHTML = `
+<div class="task-info">
 <span onclick="toggleTask(this)">${taskText}</span>
+<small>Deadline: ${deadlineDate}</small>
+</div>
+
 <div>
 <button onclick="editTask(this)">✏️</button>
 <button onclick="deleteTask(this)">❌</button>
 </div>
 `;
 
-document.getElementById("taskList").appendChild(li);
+    document.getElementById("taskList").appendChild(li);
 
-input.value="";
+    input.value = "";
+    deadline.value = "";
 }
 
-function deleteTask(btn){
-btn.parentElement.parentElement.remove();
+function deleteTask(btn) {
+    btn.parentElement.parentElement.remove();
 }
 
-function toggleTask(task){
-task.classList.toggle("completed");
+function toggleTask(task) {
+    task.classList.toggle("completed");
 }
 
-function editTask(btn){
-let task = btn.parentElement.parentElement.querySelector("span");
-let newTask = prompt("Edit tugas:", task.innerText);
+function editTask(btn) {
+    let task = btn.parentElement.parentElement.querySelector("span");
+    let newTask = prompt("Edit tugas:", task.innerText);
 
-if(newTask){
-task.innerText = newTask;
-}
+    if (newTask) {
+        task.innerText = newTask;
+    }
 }
